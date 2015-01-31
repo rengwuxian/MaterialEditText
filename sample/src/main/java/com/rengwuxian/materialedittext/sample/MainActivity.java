6e481d950bb6adaebeb7fdbb0c58183bd3e56493
Kai Zhu <rengwuxian@gmail.com>
2015-01-31 23:29:02 +0800

    removed unused imports in sample

--- sample/src/main/java/com/rengwuxian/materialedittext/sample/MainActivity.java
+++ sample/src/main/java/com/rengwuxian/materialedittext/sample/MainActivity.java
@@ -1,17 +1,12 @@
 package com.rengwuxian.materialedittext.sample;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.rengwuxian.materialedittext.validation.RegexpValidator;
