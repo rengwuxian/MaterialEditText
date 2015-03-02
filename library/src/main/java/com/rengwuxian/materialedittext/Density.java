810b3499049da05d2dec47841afe59e6d2d08110
Kai Zhu <rengwuxian@gmail.com>
2015-03-02 13:50:34 +0800

    Tweak of the dp2px(context, dp) method. Switching the casting to int behaviour to using the Math.round(int) method, to make the result more accurate, and incidently this fixes the bug that program will ANR when the MaterialEditText is set to disabled on a LDPI device.

--- library/src/main/java/com/rengwuxian/materialedittext/Density.java
+++ library/src/main/java/com/rengwuxian/materialedittext/Density.java
@@ -11,6 +11,6 @@ class Density {
   public static int dp2px(Context context, float dp) {
     Resources r = context.getResources();
     float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
-    return (int) px;
+    return Math.round(px);
   }
 }
