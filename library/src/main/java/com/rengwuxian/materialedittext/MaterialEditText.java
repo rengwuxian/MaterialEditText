1722f4324b404ddd58b18c14a57cfc0fd63503f9
kirill.rozov <kirich1409@gmail.com>
2015-04-08 23:38:27 +0300

    Add validation when view lost focus.

--- library/src/main/java/com/rengwuxian/materialedittext/MaterialEditText.java
+++ library/src/main/java/com/rengwuxian/materialedittext/MaterialEditText.java
@@ -286,6 +286,11 @@ public class MaterialEditText extends EditText {
    */
   private Bitmap[] closeButtonBitmaps;
 
+  /**
+   * Auto validate when focus lost.
+   */
+  private boolean validateOnFocusLost;
+
   private boolean showClearButton;
   private int iconSize;
   private int iconOuterWidth;
@@ -401,6 +406,7 @@ public class MaterialEditText extends EditText {
     iconPadding = typedArray.getDimensionPixelSize(R.styleable.MaterialEditText_met_iconPadding, getPixel(16));
     floatingLabelAlwaysShown = typedArray.getBoolean(R.styleable.MaterialEditText_met_floatingLabelAlwaysShown, false);
     helperTextAlwaysShown = typedArray.getBoolean(R.styleable.MaterialEditText_met_helperTextAlwaysShown, false);
+    validateOnFocusLost = typedArray.getBoolean(R.styleable.MaterialEditText_met_validateOnFocusLost, false);
     typedArray.recycle();
 
     int[] paddings = new int[]{
@@ -865,12 +871,23 @@ public class MaterialEditText extends EditText {
             getLabelFocusAnimator().reverse();
           }
         }
+        if (validateOnFocusLost && !hasFocus) {
+          validate();
+        }
         if (outerFocusChangeListener != null) {
           outerFocusChangeListener.onFocusChange(v, hasFocus);
         }
       }
     };
     super.setOnFocusChangeListener(innerFocusChangeListener);
+  }
+
+  public boolean isValidateOnFocusLost() {
+    return validateOnFocusLost;
+  }
+
+  public void setValidateOnFocusLost(boolean validate) {
+    this.validateOnFocusLost = validate;
   }
 
   public void setBaseColor(int color) {
