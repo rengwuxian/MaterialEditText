6338fcf292c192478b2894d91339653c96c86258
Ron Cai <mail@roncai.ca>
2015-06-07 17:22:01 -0400

    Add fix for MaterialAutoCompleteTextView and MaterialMultiAutocompleteTextView

--- library/src/main/java/com/rengwuxian/materialedittext/MaterialAutoCompleteTextView.java
+++ library/src/main/java/com/rengwuxian/materialedittext/MaterialAutoCompleteTextView.java
@@ -1291,7 +1291,7 @@ public class MaterialAutoCompleteTextView extends AppCompatAutoCompleteTextView 
     }
 
     // draw the clear button
-    if (hasFocus() && showClearButton && !TextUtils.isEmpty(getText())) {
+    if (hasFocus() && showClearButton && !TextUtils.isEmpty(getText()) && isEnabled()) {
       paint.setAlpha(255);
       int buttonLeft;
       if (isRTL()) {
@@ -1463,7 +1463,7 @@ public class MaterialAutoCompleteTextView extends AppCompatAutoCompleteTextView 
       setSelection(0);
       return false;
     }
-    if (hasFocus() && showClearButton) {
+    if (hasFocus() && showClearButton && isEnabled()) {
       switch (event.getAction()) {
         case MotionEvent.ACTION_DOWN:
           if (insideClearButton(event)) {
