9e41879b842f3ce27ec2abaff670c97fb458d1ed
Csaba Kozák <kozakcsabi@gmail.com>
2015-11-12 11:52:45 +0100

    Use HashMap<String, WeakReference> instead of Hashtable for TypefaceCache

--- library/src/main/java/com/rengwuxian/materialedittext/TypefaceCache.java
+++ library/src/main/java/com/rengwuxian/materialedittext/TypefaceCache.java
@@ -3,18 +3,33 @@ package com.rengwuxian.materialedittext;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 
-import java.util.Hashtable;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TypefaceCache {
-  private static final Hashtable<String, Typeface> FONT_CACHE = new Hashtable<>();
+  private static final Map<String, WeakReference<Typeface>> FONT_CACHE = new HashMap<>();
 
   public static Typeface get(String name, AssetManager assets) {
-    synchronized (FONT_CACHE) {
-      if (!FONT_CACHE.containsKey(name)) {
-        FONT_CACHE.put(name, Typeface.createFromAsset(assets, name));
-      }
-
-      return FONT_CACHE.get(name);
+    if (!FONT_CACHE.containsKey(name)) {
+      return createAndAddTypeFace(name, assets);
     }
+
+    WeakReference<Typeface> reference = FONT_CACHE.get(name);
+    Typeface typeFace = reference.get();
+
+    if (typeFace != null) {
+      return typeFace;
+    }
+
+    return createAndAddTypeFace(name, assets);
+  }
+
+  private static Typeface createAndAddTypeFace(String name, AssetManager assets) {
+    Typeface typeface = Typeface.createFromAsset(assets, name);
+
+    FONT_CACHE.put(name, new WeakReference<>(typeface));
+
+    return typeface;
   }
 }
